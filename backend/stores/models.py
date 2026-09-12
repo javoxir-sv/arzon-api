37c9a2d0ef21b387de08cdcdda666d2ad5eca396
Javokhir <javokhir.coding@gmail.com>
2026-09-12 18:37:02 +0500

    working

--- backend/stores/models.py
+++ backend/stores/models.py
@@ -1,16 +1,18 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
-
-# Create your models here.
-
 from django.conf import settings
-from django.db.models import Q, CharField
 
 User = settings.AUTH_USER_MODEL
 
 
 
 class Store(models.Model):
-    name = CharField(max_length=100, null=False, blank=False, default="Gogo Gaga")
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=400, null=True, blank=True)
+    homepage = models.URLField(null=True, blank=True)
+    image_url = models.URLField(null=True, blank=True)
+    tags = ArrayField(models.CharField(max_length=50), default=list, blank=True)
+
 
     def __str__(self):
         return self.name
